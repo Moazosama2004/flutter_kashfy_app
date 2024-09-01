20dd6e8319604ef04050c242f1cc8acf11af0b42
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2024-09-01 13:40:10 +0300

    - just a fake push to origin development

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_kashfy_app/core/routing/app_router.dart';
 import 'package:flutter_kashfy_app/doc_app.dart';
 
 void main() {
+  
   runApp(DocdocApp(
     appRouter: AppRouter(),
   ));
