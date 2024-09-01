3f9211edbde7bd14a918b9a07f992cf76c7d9027
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2024-09-01 13:42:13 +0300

    Revert "- just a fake push to origin development"
    This reverts commit 20dd6e8319604ef04050c242f1cc8acf11af0b42.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_kashfy_app/core/routing/app_router.dart';
 import 'package:flutter_kashfy_app/doc_app.dart';
 
 void main() {
-  
   runApp(DocdocApp(
     appRouter: AppRouter(),
   ));
